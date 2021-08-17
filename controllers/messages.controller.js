6069f9f2672d2ee07aed3e0ca84e2ee062c26917
Zeapherine <zeapherine@gmail.com>
2021-08-18 02:06:41 +0530

    added layout for hbs template

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,12 @@ const path = require('path');
 
 function getMessages(req, res) {
 	// sendFile needs absolute path to file.
-	res.sendFile(path.join(__dirname, '..', 'public', 'images', 'trio.JPG'));
+	// res.sendFile(path.join(__dirname, '..', 'public', 'images', 'trio.JPG'));
+
+	res.render('messages', {
+		title: 'Spell for the Dementors',
+		friend: 'Hermione Granger',
+	});
 }
 
 function postMessages(req, res) {
